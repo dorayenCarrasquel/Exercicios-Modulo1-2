df505c219d1ed498907d4f576f54fab3d0761899
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-30 14:23:36 -0300

    Finalizando o EX 001 e inicializando EX002

--- src/br/com/zup/EX001.java
+++ src/br/com/zup/EX001.java
@@ -1,8 +1,20 @@
 package br.com.zup;
+
+import java.util.Scanner;
+
 //1 - Faça um programa que mostra na tela os números de 1 a 100.
 public class EX001 {
     public static void main(String[] args) {
 
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Vamos a contar até 100");
+        System.out.println("Por favor, digite um número para iniciar o contagem: ");
+        int contador = leitor.nextInt();
+
+        while (contador <= 100){
+            System.out.println(contador);
+            contador ++;
+        }
 
 
 
