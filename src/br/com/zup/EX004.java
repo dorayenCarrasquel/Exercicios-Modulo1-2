7fb626d848eb207a49dcf094f0a5e2d2fff62029
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-30 14:59:39 -0300

    finalizando EX 004

--- src/br/com/zup/EX004.java
+++ src/br/com/zup/EX004.java
@@ -7,6 +7,26 @@ public class EX004 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
+        int quantidaDeNumeros = 0;
+        int contador = 1;
+
+        double soma = 0;
+        double media = 0;
+        double valorDigitado = 0;
+
+        System.out.println("Por favor, Digite quantos números va a avaliar para a media: ");
+        quantidaDeNumeros = leitor.nextInt();
+
+        while (contador <= quantidaDeNumeros){
+            System.out.println("Por favor digite o " +contador +"º número");
+            double valordoUsuario = leitor.nextDouble();
+
+            soma = soma + valordoUsuario;
+            System.out.println(" a suma atual é: "+soma);
+            contador ++;
+        }
+        media = soma /  quantidaDeNumeros;
+        System.out.println("a media dos "+ (contador -1) +" números é de: "+media);
 
     }
 }
