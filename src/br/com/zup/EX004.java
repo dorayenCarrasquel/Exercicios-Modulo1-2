12669dcffbe0f5b932d12c34242a07931705bb40
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-30 14:46:26 -0300

    Adicionando EX 004

---
+++ src/br/com/zup/EX004.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+//4 - Escreva, usando while, um programa para calcular a média de N números. O valor de N é dado pelo usuário.
+public class EX004 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+
+    }
+}
