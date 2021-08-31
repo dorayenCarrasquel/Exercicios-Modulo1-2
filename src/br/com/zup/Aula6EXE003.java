dc2cae8854283b6409bca186a026e0e3cac8d72f
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-31 15:42:45 -0300

    Exercicios Lista 7 31/Ago

--- src/br/com/zup/Aula6EXE003.java
+++ src/br/com/zup/Aula6EXE003.java
@@ -1,4 +1,39 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Aula6EXE003 {
+    public static void main(String[] args) {
+        List<Integer> todosOsNumeros = new ArrayList<>();
+        List<Integer> numerosPares = new ArrayList<>();
+        List<Integer> numerosImpares = new ArrayList<>();
+
+        todosOsNumeros.add(10);
+        todosOsNumeros.add(12);
+        todosOsNumeros.add( 5);
+
+        for (Integer referencia:todosOsNumeros) {
+            if (referencia % 2 == 0) {
+                numerosPares.add(referencia);
+            } else {
+                numerosImpares.add(referencia);
+            }
+
+        }
+        System.out.println("Analizando " + todosOsNumeros.size() + " números");
+        System.out.println( "********************");
+        System.out.println("* Numeros Impares *");
+        System.out.println( "********************");
+        System.out.println("Quantidade de Numeros Impares: " + numerosImpares.size());
+        System.out.println("Os numeros impares foram: " + numerosImpares);
+        System.out.println( "********************");
+        System.out.println("* Numeros pares *");
+        System.out.println( "********************");
+        System.out.println("Quantidade de Numeros pares: " + numerosPares.size());
+        System.out.println("Os numeros pares foram: " + numerosPares);
+        System.out.println( "********************");
+
+    }
 }
---
+++ src/br/com/zup/L7EX001.java
@@ -0,0 +1,30 @@
+package br.com.zup;
+//1 - Escreva um programa que escreva na tela,
+// de 1 até 100, de 1 em 1, 2 vezes. A primeira
+// vez com "for" e a segunda com "while".
+public class L7EX001 {
+    public static void main(String[] args) {
+
+        int contador = 1;
+        int vez = 1;
+        while (vez<= 2){
+            if (vez <= 1){
+                for (contador = 1; contador <= 100; contador++) {
+                    System.out.print(contador + " - ");
+                    vez=+1;
+                }
+            }
+            else {
+                while (contador<= 100){
+                    System.out.println(contador +" :D");
+                    contador++;
+
+                }
+            }
+            vez++;
+            contador = 0;
+        }
+
+
+    }
+}
