8c1d80f0d0f9cd05754fe490cb639d4da3157ccb
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-31 14:27:24 -0300

    Fazendo Exercicio num Pares e Impares

--- src/br/com/zup/Aula6EXE002.java
+++ src/br/com/zup/Aula6EXE002.java
@@ -1,7 +1,11 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aula6EXE002 {
     public static void main(String[] args) {
+        List<String> alunos = new ArrayList<>();
 
     }
 }
---
+++ src/br/com/zup/Aula6EXE003.java
@@ -0,0 +1,4 @@
+package br.com.zup;
+
+public class Aula6EXE003 {
+}
