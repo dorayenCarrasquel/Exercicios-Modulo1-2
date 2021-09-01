23dc3e924841cbb924dd49a4800597a14e2015db
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-01 15:53:31 -0300

    Exercicio 006 Lista 7

--- src/br/com/zup/lista7/L7EX006.java
+++ src/br/com/zup/lista7/L7EX006.java
@@ -1,4 +1,31 @@
 package br.com.zup.lista7;
 
+import java.util.Scanner;
+
+//Faça um programa que some os números ímpares
+// contidos em um intervalo definido pelo usuário.
+// O usuário define o valor inicial do intervalo
+// e o valor final deste intervalo e o programa
+// deve somar todos os números ímpares contidos
+// neste intervalo. Caso o usuário digite um
+// intervalo inválido (começando por um valor
+// maior que o valor final) deve ser escrito uma
+// mensagem de erro na tela, “Intervalo de
+// valores inválido” e o programa termina.
 public class L7EX006 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int numImpares = 0;
+        int valorInicial = 0;
+        int valorFinal = 0;
+        int soma = 0;
+
+        //
+        System.out.println("Por favor, indique um valor inicial para o intervalo: ");
+        valorInicial = leitor.nextInt();
+        System.out.println("por favor, indique um valor final para seu intervalo: ");
+        valorFinal = leitor.nextInt();
+
+    }
 }
